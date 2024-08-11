c497f5c7a14c637a6b43c15e33d93f0067049b8d
Artem <artkrasov@gmail.com>
2024-08-11 17:03:07 +0300

    Добавьте файлы проекта.

---
+++ WpfApp5/FileManager.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для FileManager.xaml
+    /// </summary>
+    public partial class FileManager : Window
+    {
+        private readonly UserViewModel userViewModel = new();
+        public FileManager()
+        {
+            InitializeComponent();
+            DataContext = userViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(WriteToFile);
+            thread.Start();
+        }
+
+        public async Task WriteToFileAsync()
+        {
+            using var fileStream = new FileStream(@"C:\test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            for (int i = 0; i < 1_000_000_000; i++)
+            {
+                byte[] buffer = Encoding.Default.GetBytes(userViewModel.ToString());
+                await fileStream.WriteAsync(buffer, 0, buffer.Length);
+            }
+        }
+
+        public void SomeMethod()
+        {
+            var thread = new Thread(PlayVideo);
+            thread.Start();
+        }
+
+        public void PlayVideo()
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            Close();
+        }
+    }
+}
---
+++ WpfApp5/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    internal class UserViewModel
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Имя: {Name} Фамилия: {Surname} Отчество: {Patronymic} Email: {Email}";
+        }
+    }
+}
